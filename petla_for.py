42521c2a15eb22bb1ac55b51207edcce13c4576c
Tomasz Pawłowski <t.pawlowski@getback.ad2008r2.corp>
2018-05-02 15:25:10 +0200

    używanie pętli for do tworzenia prostej listy mailingowej

---
+++ petla_for.py
@@ -0,0 +1,8 @@
+# tworzenie listy mailowej używając pętli for
+
+persons = ['Elizabeth', 'Steven', 'Sebastian', 'Margaret', 'Svetlana', 'Raphael']
+domain = 'mycompany.com'
+
+for person in persons:
+    email = person + '@' + domain
+    print('Email for\t', person, '\tis\t', email)
