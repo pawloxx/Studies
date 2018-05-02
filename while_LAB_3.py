e06361ae506ccf686603100739651a444e99f39a
pawloxx <murinus.dot@gmail.com>
2018-05-02 20:44:37 +0200

    Zadanie 1 - while LAB

--- while_LAB_3.py
+++ while_LAB_3.py
@@ -15,3 +15,16 @@ the application is developed faster, and unit testing each piece is easier.
 policz ile jest w nim słów dłuższych od wordLength = 6
 '''
 
+slowa = 'United Space Alliance: This company provides major support to NASA for' \
+        'various projects, such as the space shuttle. One of its projects is to' \
+        'create Workflow Automation System (WAS), an application designed to' \
+        'manage NASA and other third-party projects. The setup uses a central' \
+        'Oracle database as a repository for information. Python was chosen over' \
+        'languages such as Java and C++ because it provides dynamic typing and' \
+        'pseudo-code–like syntax and it has an interpreter. The result is that' \
+        'the application is developed faster, and unit testing each piece is easier'
+
+##print(slowa.split(' '))
+listaSlow = slowa.split(' ')
+
+print(listaSlow)
