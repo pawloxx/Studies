bef0655ebe17dc324b86c8f3e4e59611d634d725
pawloxx <murinus.dot@gmail.com>
2018-05-04 11:52:47 +0200

    sprawdzanie za pomocą pętli czy wyświetlona liczba jest parzysta

--- for_numbers_range.py
+++ for_numbers_range.py
@@ -1,6 +1,6 @@
 # sprawdzanie za pomocą pętli czy wyświetlona liczba jest parzysta
 
-for number in range(1, 101):
+for number in range(1, 10):
     if number % 2 == 0:
         print('Number %2d is %s' % (number, 'even'))
     else:
