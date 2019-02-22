0f5c2349eb9fa67a3aa8fcbf0f944b954a04fa85
pawloxx <murinus.dot@gmail.com>
2019-02-22 13:43:59 +0100

    lista mailingowa, pętla for, instrukcja continue

--- takietam.py
+++ takietam.py
@@ -1,1 +1,4 @@
-print(9 % 3)
+dictionary={'A':'80%-10%','B':'60%-80%','C':'50-60%','D':'less then 50%'}
+for word in dictionary.keys():
+    print(word,'-',dictionary[word])
+print('-----------------------------------------------')
