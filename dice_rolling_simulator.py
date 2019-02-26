2f8bd1d97cdee03d18347460521039b68c830f35
pawloxx <murinus.dot@gmail.com>
2019-02-26 11:05:18 +0100

    nowe próby

--- dice_rolling_simulator.py
+++ dice_rolling_simulator.py
@@ -1,30 +1,10 @@
-"""1. Dice Rolling Simulator
-
-The Goal: Like the title suggests, this project involves writing a program that simulates rolling dice.
-When the program runs, it will randomly choose a number between 1 and 6. (
-Or whatever other integer you prefer — the number of sides on the die is up to you.)
-The program will print what that number is. It should then ask you if you’d like to roll again.
-For this project, you’ll need to set the min and max number that your dice can produce.
-For the average die, that means a minimum of 1 and a maximum of 6.
-You’ll also want a function that randomly grabs a number within that range and prints it.
-
-Concepts to keep in mind:
-
-    Random
-    Integer
-    Print
-    While Loops
-
-
-A good project for beginners, this project will help establish a solid foundation for basic concepts.
-And if you already have programming experience, chances are that the concepts used in this project aren’t
-completely foreign to you. Print, for example, is similar to Javascript’s console.log."""
-def kostka():
+"""prosta, staropolska kostka do losowania, argument określa max zakres"""
+def kostka(x):
     from random import randint
     inoroz = True
     while inoroz:
-        print("wypadlo",randint(1,6))
+        print("wypadlo",randint(1,x))
         print("Inoroz?")
-        inoroz = ("t" or "tak" or "ja") in input().lower()
+        inoroz = ("t" or "tak") in input().lower()
 
-kostka()
+kostka(100)
